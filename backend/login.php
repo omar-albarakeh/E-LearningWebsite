9cce48d2ca7e53a352f0f68332eb3d9bd4643d03
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-21 01:26:52 +0200

    php <> login page request method check added

--- backend/login.php
+++ backend/login.php
@@ -1,8 +1,12 @@
-</php
+<?php
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include_once 'db_connection.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 ?>
