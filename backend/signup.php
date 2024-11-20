ccf046751650e4e6123ac04aeaa7716772e2d6c5
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-21 01:33:52 +0200

    php <> signup page added

---
+++ backend/signup.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+include_once 'db_connection.php';
+
+?>
