ecd950834dea83dc745e44a1f4b2855494508d95
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-21 01:50:31 +0200

    react <> signup usesatae added

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -3,6 +3,10 @@ import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../styles/styles.css';
 const signup = () => {
+  const [signupForm, setSignupForm] = useState({ username: '', password: '' });
+  const [message, setMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div>signup</div>
   )
