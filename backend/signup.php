e44dbda6cfc629e9ddef356d2f57d9d01eae7612
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-21 01:34:28 +0200

    php <> signup request method check added

--- backend/signup.php
+++ backend/signup.php
@@ -5,4 +5,10 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include_once 'db_connection.php';
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+
 ?>
