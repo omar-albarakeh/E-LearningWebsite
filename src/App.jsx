b9381cc7ae097f36e13f9a2f37c8c2fe6af7cfe1
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-21 01:47:44 +0200

    react <> App.jsx updated

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/login';
+import Signup from './components/signup';
+import Student from './components/student';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/student" element={<Student />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
