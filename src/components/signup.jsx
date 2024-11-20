85ce865594b5c5728f6106278937fe3d18a9e038
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-21 01:49:51 +0200

    react <> signup component added

---
+++ src/components/signup.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom'; 
+import axios from 'axios';
+import '../styles/styles.css';
+const signup = () => {
+  return (
+    <div>signup</div>
+  )
+}
+
+export default signup
