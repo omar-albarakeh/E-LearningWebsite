e8cf387703be17b4fe4407c8d5607a0cb23a155a
omar albarakeh <omaralbarakeh2@gmail.com>
2024-11-21 12:37:25 +0200

    react <> footer component added

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; {new Date().getFullYear()} E-Learning Platform. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
